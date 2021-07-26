5f3e4607b0be0fbacb2973da0445bc5ac18df091
tosmel <83775651+tosmel@users.noreply.github.com>
2021-07-26 19:29:31 +0800

    _printf

--- _printf.c
+++ _printf.c
@@ -9,28 +9,28 @@
 
 int _printf(const char *format, ...)
 {
-  va_list args;
-  int size = 0;
-  print_fx fx[] = {
-		   {"c", print_c},
-		   {"s", print_s},
-		   {"i", print_i},
-		   {"d", print_i},
-		   {"u", print_i},
-		   {"b", print_b},
-		   {"o", print_o},
-		   {"x", print_x},
-		   {"X", print_X},
-		   {"r", print_r},
-		   {"R", print_R},
-		   {NULL, NULL}
-  };
-  va_start(args, format);
-  if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-    {
-      return (-1);
-    }
-  size = aux_func(format, args, fx);
-  va_end(args);
-  return (size);
+	va_list args;
+	int size = 0;
+	print_fx fx[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{"i", print_i},
+		{"d", print_i},
+		{"u", print_i},
+		{"b", print_b},
+		{"o", print_o},
+		{"x", print_x},
+		{"X", print_X},
+		{"r", print_r},
+		{"R", print_R},
+		{NULL, NULL}
+	};
+	va_start(args, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+	{
+		return (-1);
+	}
+	size = aux_func(format, args, fx);
+	va_end(args);
+	return (size);
 }
