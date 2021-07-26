3d898e0943cfc32b3c08ca565c9bc08d1ef2f3bf
tosmel <83775651+tosmel@users.noreply.github.com>
2021-07-26 19:31:56 +0800

    aux_func

--- aux_func.c
+++ aux_func.c
@@ -11,44 +11,44 @@
 
 int aux_func(const char *format, va_list args, print_fx *fx)
 {
-  int i = 0, j, size = 0, test = 0;
+	int i = 0, j, size = 0, test = 0;
 
-  while (format && format[i])
-    {
-      if (format[i] == '%')
+	while (format && format[i])
 	{
-	  j = 0;
-	  while (fx[j].c != NULL)
-	    {
-	      if (format[i + 1] == '%')
+		if (format[i] == '%')
 		{
-		  write(1, &format[i], 1);
-		  size++;
-		  i++;
-		  test = 1;
-		  break;
+			j = 0;
+			while (fx[j].c != NULL)
+			{
+				if (format[i + 1] == '%')
+				{
+					write(1, &format[i], 1);
+					size++;
+					i++;
+					test = 1;
+					break;
+				}
+				else if (fx[j].c[0] == format[i + 1])
+				{
+					size += fx[j].f(args);
+					i++;
+					test = 1;
+					break;
+				}
+				j++;
+			}
+			if (test == 0)
+			{
+				write(1, &format[i], 1);
+				size++;
+			}
 		}
-	      else if (fx[j].c[0] == format[i + 1])
+		else
 		{
-		  size += fx[j].f(args);
-		  i++;
-		  test = 1;
-		  break;
+			write(1, &format[i], 1);
+			size++;
 		}
-	      j++;
-	    }
-	  if (test == 0)
-	    {
-	      write(1, &format[i], 1);
-	      size++;
-	    }
+		i++;
 	}
-      else
-	{
-	  write(1, &format[i], 1);
-	  size++;
-	}
-      i++;
-    }
-  return (size);
+	return (size);
 }
