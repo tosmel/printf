7d0f3fa7032c7d6e32459034cab48bc4cf7bb7f8
tosmel <tosmeltungsten@gmail.com>
2021-07-26 04:17:45 -0700

    _printf

---
+++ _printf.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * _printf - prints formated text
+ *
+ * @format: text to be formated
+ * Return: Lenght of the text
+ */
+
+int _printf(const char *format, ...)
+{
+  va_list args;
+  int size = 0;
+  print_fx fx[] = {
+		   {"c", print_c},
+		   {"s", print_s},
+		   {"i", print_i},
+		   {"d", print_i},
+		   {"u", print_i},
+		   {"b", print_b},
+		   {"o", print_o},
+		   {"x", print_x},
+		   {"X", print_X},
+		   {"r", print_r},
+		   {"R", print_R},
+		   {NULL, NULL}
+  };
+  va_start(args, format);
+  if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+    {
+      return (-1);
+    }
+  size = aux_func(format, args, fx);
+  va_end(args);
+  return (size);
+}
